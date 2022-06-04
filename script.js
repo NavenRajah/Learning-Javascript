14162633e918230b412550517c00278b4827dc52
NavenRajah <65741660+NavenRajah@users.noreply.github.com>
2022-06-04 19:10:24 +0200

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+/*const day = 'Thursday';
+
+switch (day) {
+    case 'Monday':
+        console.log('Go to coding Meeting');
+}
+
+const bill = 275;
+const tip = bill <= 275 && bill >=75 ? bill * 0.15 : bill *0.2;
+
+console.log(`the bill was ${bill}, the tip was ${tip}', and the total value ${bill + tip}`); */
